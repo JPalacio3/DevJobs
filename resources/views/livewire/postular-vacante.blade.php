2711da461fb46209f3411e5e0d88ee798f45fc68
unknown <joelpalacio630@gmail.com>
2024-09-07 16:32:26 -0600

    feat : Añadiendo el componente del formulario para subir el CV y aplicar a una vacante

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,11 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
-    <h3 class="text-center text-2xl font-bold my-4"></h3>
+    <h3 class="text-center text-2xl font-bold my-4">Postularme a esta Vacante</h3>
+
+    <form action="" class="w-96 mt-5">
+
+        <div class="mb-4">
+            <x-input-label for="cv" :value="__('Curricullum u Hoja de Vida ( PDF )')" />
+            <x-text-input id="cv" class=" my-10 block mt-1 w-full" type="file" wire:model="imagen" accept=".pdf" />
+        </div>
+    </form>
 </div>
