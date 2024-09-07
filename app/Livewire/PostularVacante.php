2147c78548aef1526973369b3cb2520f7fe92761
unknown <joelpalacio630@gmail.com>
2024-09-07 16:59:33 -0600

    feat : Implementando la validación del campo de cv desde el controlador

--- app/Livewire/PostularVacante.php
+++ app/Livewire/PostularVacante.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function postularme()
+    {
+        // Añadir la validación al campo de cv
+        $this->validate();
+
+        // Almacenar CV en el disco duro
+
+        // Crear la postulación
+
+        // Crear notificación y enviar email de confirmación
+
+        // Mostrar al usuario un mensaje de postulación exitosa
+
+    }
+
+
     public function render()
     {
         return view('livewire.postular-vacante');
